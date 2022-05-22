4d60e7d23210ee357ddb52d3c70ba0df60e412d8
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2022-05-22 12:14:16 +0700

    import R.layout splashscreen

--- app/src/main/java/com/dicoding/emodiary/ui/view/SplashScreenActivity.kt
+++ app/src/main/java/com/dicoding/emodiary/ui/view/SplashScreenActivity.kt
@@ -2,6 +2,7 @@ package com.dicoding.emodiary.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.emodiary.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
